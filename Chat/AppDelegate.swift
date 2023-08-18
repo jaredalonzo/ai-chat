2665b02b3662e1e010ac84a32afec68470b8cd89
Jared Alonzo <jared.alonzo@icloud.com>
2023-08-18 12:18:27 -0400

    Initial Commit

---
+++ Chat/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Chat
+//
+//  Created by Jared Alonzo on 8/18/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
